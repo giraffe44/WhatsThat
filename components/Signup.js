d313a0346c0473a71b93f953febc8f9d61c2b55a
giraffe44 <rahmah.rifat@gmail.com>
2023-05-31 23:59:08 +0100

    improve usability, make consistent with login page

--- components/Signup.js
+++ components/Signup.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text } from 'react-native';
+import { View, Button, Text } from 'react-native';
 import { CREATE_USER } from '../config';
+import TextInput from './subcomponents/TextInput.js'
 
-const createUser = async (firstName, lastName, email, password, setError) => {
+const createUser = async (firstName, lastName, email, password, setError) => {
   try {
     const response = await fetch(CREATE_USER, {
       method: 'POST',
@@ -25,57 +26,77 @@ const createUser = async (firstName, lastName, email, password, setError) => {
       return
     }
 
-    const responseJson = await response.json()
-    console.log('Response:', responseJson);
+    // navigation.navigate('Login')
+    // const responseJson = await response.json()
+    // console.log('Response:', responseJson);
 
   } catch (e) {
     console.log(e)
   }
 }
 
-const Signup = ({navigation}) => {
+const Signup = ({ navigation }) => {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
 
+  const checkPassword = () => {
+    if (password === '') return true
+
+    const password_regex = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+    return password_regex.test(password)
+  }
+  const isValidPassword = checkPassword()
+
   return (
-    <View>
+    <View style={{ marginHorizontal: 40 }}>
       <TextInput
+        marginTop={100}
         placeholder="Your first name"
         onChangeText={(text) => { setFirstName(text) }}
         value={firstName}
       />
       <TextInput
+      marginTop={20}
         placeholder="Your last name"
         onChangeText={(text) => { setLastName(text) }}
         value={lastName}
       />
       <TextInput
+        marginTop={20}
         placeholder="Your email"
         onChangeText={(text) => { setEmail(text) }}
         value={email}
       />
       <TextInput
+        marginTop={20}
         placeholder="Choose a password"
+        secureTextEntry={true}
         onChangeText={(password) => { setPassword(password) }}
         value={password}
+        error={isValidPassword ? null : "Password must be strong (greater than 8 characters inclusing one upper, one number, and one special character."}
       />
 
-      {!!error && <Text>{error}</Text>}
+      {!!error && <Text style={{marginTop: 20, color: 'red'}}>{error}</Text>}
 
-      <Button
-        title="Create" 
-        onPress={() => {
-          createUser(firstName, lastName, email, password, setError)
-        }}
-      />
-      <Text
+      <View style={{ marginBottom: 40, marginTop: 40 }}>
+        <Button
+          title="Create"
+          onPress={() => {
+            createUser(firstName, lastName, email, password, setError)
+          }}
+        />
+      </View>
+      <View>
+        <Text
           onPress={() => navigation.navigate('Login')}
-        > 
+          style={{ textAlign: 'center', color: '#333', fontSize: 18 }}
+        >
           Already have an account? Login here
         </Text>
+      </View>
     </View>
   );
 
