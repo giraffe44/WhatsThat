b56dded5a4ae4101bfd1383388d0b60608258555
giraffe44 <rahmah.rifat@gmail.com>
2023-06-01 00:00:18 +0100

    fixed photo component on android

--- components/UploadPhoto.js
+++ components/UploadPhoto.js
@@ -1,56 +1,68 @@
-import React, { useEffect, useState, useContext, useRef } from 'react';
-import { View, TextInput, Button, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useContext, useRef } from 'react';
+import { View, Text, TouchableOpacity, Button } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 import { UPDATE_USER_PHOTO } from '../config'
-import { AuthContext } from '../App';
+import { AuthContext } from '../App'; 
+import { useIsFocused } from '@react-navigation/native';
 
-
-const UserPhoto = () => {
+const UserPhoto = ({navigation}) => {
   const [type, setType] = useState(CameraType.back);
   const cameraRef = useRef(null);
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const { userToken, userId } = useContext(AuthContext);
+  const isFocused = useIsFocused()
 
-  // TODO: check permissions
+  if (!permission) {
+    requestPermission()
+  }
 
   const toggleCameraType = () => {
     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
 
-  const takePhoto = async (props) => {
-    const photo = await cameraRef.current.takePictureAsync({ quality: 0.5, base64: true });
+  const takePhoto = async () => {
+    try {
+      const photo = await cameraRef.current.takePictureAsync({ quality: 0.5, base64: true });
 
-    // Convert from base64 to blob using the fetch api
-    var url = photo.uri;
-    const photoBlob = await fetch(url).then(res => res.blob())
+      // Convert from base64 to blob using the fetch api
+      const url = photo.uri;
+      const photoBlob = await fetch(url).then(res => res.blob())
 
-    fetch(UPDATE_USER_PHOTO(userId), {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'image/png',
-        'Content-Transfer-Encoding': 'base64',
-        'X-Authorization': userToken
-      },
-      body: photoBlob
-    })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+      fetch(UPDATE_USER_PHOTO(userId), {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'image/png',
+          'Content-Transfer-Encoding': 'base64',
+          'X-Authorization': userToken
+        },
+        body: photoBlob
+      })
+        .catch(error => {
+          console.error('Error blob:', error);
+        });
+
+    } catch (error) {
+      console.error('Error takePhoto:', error);
+    }
   }
 
 
   return (
     <View>
-      <Camera style={{ minHeight: 300 }} type={type} ref={cameraRef}>
-        <View>
-          <TouchableOpacity onPress={toggleCameraType}>
-            <Text>Flip Camera</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={takePhoto}>
-            <Text>Take Photo</Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
+       { isFocused && (<Camera style={{ minHeight: 300 }} type={type} ref={cameraRef} />)}
+      <View>
+        <TouchableOpacity onPress={toggleCameraType}>
+          <Text style={{ fontSize: 20, marginTop: 10, textAlign: 'center' }}>Flip Camera</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={takePhoto}>
+          <Text style={{ fontSize: 20, marginTop: 10, textAlign: 'center' }}>Take Photo</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Button title="Update your profile" onPress={() => {
+          navigation.push('Profile')
+        }} />
+      </View>
     </View>
   );
 
